7a616ce10eb5ca54a103a6d66d20f0ee6cb29505
Welder Leite Garrido <weldergarrido@gmail.com>
2024-11-10 23:31:16 -0300

    inserção do enunciado

--- Sprint2/Exercicios/ex08.sql
+++ Sprint2/Exercicios/ex08.sql
@@ -1,3 +1,6 @@
+/*Apresente a query para listar o código e o nome do vendedor com maior número de vendas (contagem), 
+e que estas vendas estejam com o status concluída.  As colunas presentes no resultado devem ser, portanto, cdvdd e nmvdd.*/
+
 SELECT 
 	v.cdvdd,
 	ven.nmvdd
