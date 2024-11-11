ed7c4489c59ee17b42175f1abed037bc6ee13626
Welder Leite Garrido <weldergarrido@gmail.com>
2024-11-11 17:53:58 -0300

    inserção de comentários no código

--- Sprint2/Desafio/etapa-2/criacaoDim.sql
+++ Sprint2/Desafio/etapa-2/criacaoDim.sql
@@ -1,6 +1,6 @@
-DROP TABLE tabelaFato;
+DROP TABLE tabelaFato; -- derrubar tabela caso existe para fazer os testes
 
-CREATE TABLE tabelaFato(
+CREATE TABLE tabelaFato( -- criação da tabela tabelaFato
 	idCliente 	INT			NOT NULL,
 	idVendedor 	INT 		NOT NULL,
 	idCarro 	INT 		NOT NULL,
@@ -10,11 +10,11 @@ CREATE TABLE tabelaFato(
 	horaEntrega TIME 		NOT NULL,
 	qtdDiaria 	INT			NOT NULL,
 	vlrDiaria 	DECIMAL		NOT NULL,
-	PRIMARY KEY (idCliente,idVendedor,idCarro,dataLocacao)
-	FOREIGN KEY (idCliente) 	REFERENCES dim_cliente(id),
-    FOREIGN KEY (idVendedor) 	REFERENCES dim_vendedor(id),
-    FOREIGN KEY (idCarro) 		REFERENCES dim_carro(id),
-    FOREIGN KEY (idCarro)		REFERENCES dim_data(id)
+	PRIMARY KEY (idCliente,idVendedor,idCarro,dataLocacao) -- chave primária composta
+	FOREIGN KEY (idCliente) 	REFERENCES dim_cliente(id), -- chave estrangeira para ligar a tabela dim_cliente à tabelaFato
+    FOREIGN KEY (idVendedor) 	REFERENCES dim_vendedor(id), -- chave estrangeira para ligar a tabela dim_vendedor à tabelaFato
+    FOREIGN KEY (idCarro) 		REFERENCES dim_carro(id), -- chave estrangeira para ligar a tabela dim_carro à tabelaFato
+    FOREIGN KEY (idCarro)		REFERENCES dim_data(id) -- chave estrangeira para ligar a tabela dim_data à tabela fato
 );
 
 /*-------------------------------------------------------------------------------------*/
@@ -22,7 +22,7 @@ CREATE TABLE tabelaFato(
 DROP TABLE dim_cliente;
 
 CREATE TABLE dim_cliente(
-    id       INT PRIMARY KEY 	NOT NULL,
+    id       INT PRIMARY KEY 	NOT NULL, -- chave primária, identificador único
     nome     VARCHAR(100) 		NOT NULL,
     cidade   VARCHAR(40) 		NOT NULL,
     estado   VARCHAR(40) 		NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE dim_cliente(
 DROP TABLE dim_carro;
 
 CREATE TABLE dim_carro(
-	id         		INT 		PRIMARY KEY 		NOT NULL,
+	id         		INT 		PRIMARY KEY 		NOT NULL, -- chave primária, identificador único
     km         		INT 							NOT NULL,
     classi     		VARCHAR(50) 					NOT NULL,
     marca      		VARCHAR(80) 					NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE dim_data(
 	diaEntrega 				INT			,
 	mesEntrega				INT			,
 	anoEntrega				INT			,
-	PRIMARY KEY (id, dataLocacao)
+	PRIMARY KEY (id, dataLocacao) -- chave primária composta
 );
 
 /*-------------------------------------------------------------------------------------*/
