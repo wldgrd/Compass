60a59cd6b2a3d5dfdade6aa5f5e2a366c5741bd0
Welder Leite Garrido <weldergarrido@gmail.com>
2024-11-14 11:54:54 -0300

    ex14

--- Sprint3/Exercicios/ex14.py
+++ Sprint3/Exercicios/ex14.py
@@ -2,3 +2,15 @@
 e um número variado de parâmetros nomeados e imprime o valor de cada parâmetro recebido.
 Teste sua função com os seguintes parâmetros:
 (1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)'''
+
+def func(*nao_nomeados, **nomeados):
+    #print('Não nomeados: ', len(nao_nomeados))
+    for valor in nao_nomeados:
+        print(valor)
+
+    
+    #print('Nomeados: ', len(nomeados))
+    for chave, valor in nomeados.items():
+        print(f'{valor}')
+
+func(1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)
