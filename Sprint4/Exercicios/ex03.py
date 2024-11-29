37a849b2bf1082a437dd5ef3c512698304138f31
Welder Leite Garrido <weldergarrido@gmail.com>
2024-11-29 13:57:06 -0300

    criação ex03

---
+++ Sprint4/Exercicios/ex03.py
@@ -0,0 +1,16 @@
+'''A função calcula_saldo recebe uma lista de tuplas, correspondendo a um conjunto de lançamentos bancários. 
+Cada lançamento é composto pelo seu valor (sempre positivo) e pelo seu tipo (C - crédito ou D - débito). 
+Abaixo apresentando uma possível entrada para a função.
+
+lancamentos = [
+    (200,'D'),
+    (300,'C'),
+    (100,'C')
+]
+A partir dos lançamentos, a função deve calcular o valor final, somando créditos e subtraindo débitos. Na lista anterior, por exemplo, 
+teríamos como resultado final 200.
+Além de utilizar lambdas, você deverá aplicar, obrigatoriamente, as seguintes funções na resolução:
+
+reduce (módulo functools)
+map'''
+
