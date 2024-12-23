e1c1ea692073d65c0d12e9f36fb568e5d871bac4
Welder Leite Garrido <weldergarrido@gmail.com>
2024-12-23 12:16:17 -0300

    Alteração de variável

--- Sprint5/Desafio/script1_cria_envia.py
+++ Sprint5/Desafio/script1_cria_envia.py
@@ -2,9 +2,9 @@ import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
 
 # Configurações de credenciais
-aws_access_key_id = 'ASIAWPPO6JPYL4BVLIOX'
-aws_secret_access_key = 'CvYqg6K25Or9olbH78OKJfWc2qI5E/K/esAaLeb1'
-session_token = 'IQoJb3JpZ2luX2VjEAQaCXVzLWVhc3QtMSJHMEUCIQCC3S4BAlkACzGwMDAq5++IEweWuSfDoYF6klEuL5XuLwIgFZWZMX3ldg7zXEpVTQq3RoyVuFMXot/4xc/n9q6+vLwqqgMIzf//////////ARAAGgw0NDU1NjcwOTM3NDQiDIIhFVuL/dYPKcHLuCr+ArB1ZlfJQRUBNtTcFyGUZolqDakW5UEN4+sHMTKXGtPxzFuYd/lzRpdEbkkn8kgXsOK1h4xTm4Bzcjb8Awfz4Td1BAkYXBs1TdxD6x0YhXktXoE84ugKhov3uXZGP0C7cIhFKlRo9IgGav3W1xMU7ebbYIGZoIkjfrZIVIBCUfEqI72BCDeNGwdbri1uqjyz5xLHJRNiWLk5F4CEAAOeKUO5VSNysU2Rzj9flK9yntUt2g7swZhOyDjw4pd+SbmhE9lOT/frCO8njEFpRh4LRpeFAkKdoUdyHL8O99qkSbPk6kuDVujl+8b1C7JZsJSYzj6NLSpIU/wT0530Ii5bt9Y5+XVtP5B4rg03q4LIaj3HwUJ8AQbla6slBzjzuCAMBcbKR7Qm2i3EKI0gB2o3Hknfp2UQpyx9vdGuH3sLeaq4Nx0gRYWHcZE95g+fSs82BU81+DPi9PpbWb1TMIU50+2PVlo79UgFSNc9rbRvxG1iCuwZNKDv+amHoOZBpMkw3bOjuwY6pgFXFj7iZIPXr28BzdTAGkXcebNMXEnuLyKllCqz3Fiwo7/wLb2fosAjCsBMAzsG1pWGBlQp53QXARFnVqPT/RS4j0Dgb8zEj65Vzj4X+XcqHjhjf1dRXR7E3uvg0jK8SmNgTUxbLsQggqY+TFVtLG6rbx+FUiSD6wOz4sZQSqqTtug7Dxrlw0X4bsQcmhJV4locWlBfW5yCu9rNsELtJ8uuxdYTmXIi'
+aws_access_key_id = 'ASIAWPPO6JPYDI4PWN4J'
+aws_secret_access_key = '8LGL2c6smjKi14nafGkSnWBbaUTD++oA31FUPewV'
+session_token = 'IQoJb3JpZ2luX2VjEA8aCXVzLWVhc3QtMSJGMEQCICZ4ZJEDNIcgyxuevdK+SCsNdzf35MqxdHLPvXPw6e4uAiAf1UwwqIWXk6cPit0bVcGLlLr/xy+EDaX4cUxWVO+HQyqqAwjY//////////8BEAAaDDQ0NTU2NzA5Mzc0NCIM8oEgKde1d+ysuexxKv4CnFswsgNSVTrFEjIWBgnlZGIJJuj4XkDpkwmjZ94SRLgij/DMAqDmCmJD6MejE1RoYEks0B6X7BwZbmUnO3/eqoGd8y185hj0pxE7UFa3vxrnn1JKQ9EyuBaJnBUYqndWaojF6sW7SxmtJKcoP8iDcPiFFY82YfwWByOAgBzFRJhAuRg8HDawIemPdt9w/DZmVly3XbrbheaZScAO7TdWESRWlLkbyEYdD8tvxGelGsA6m/HI/j7z96Tlb6S9k9a6Ht3S6SQRect5YPEJdsNv4pmOl1mbFYjQBmBnDI9zFZiC7aCzpVMUW35ONtYPTH1jLSL0EGjcRNNdvMSMf7sJhk4FXLNa3kyxPW4dZhILk+Np85UHmXYO2HAmvT6q5/OeXvKw1Sz42HK5ccLoA/YpS+VJUK4GJkK4wosU7+K+msOVTVQcY1imSlZp+bYSXE9CAgDmhSn05W+8/bdTcc0JDdT8jR795QfmRr8y6HvaqJOg0jrotIzuhfAsDhmJ7TD77aW7BjqnASUtoZJbN3n7Vm/ibed3yDJrb2yCa7MJg7yYokyJIn++bIqYuxHGInTcUniOfKtjUWE1BiadfwUE0zrVQmOuhBMfXJ5fbiphOpzduUnYXw6p0WXobF7JeLjYWM4gjx3Y+9tBFXpIwV2gzyH5yCECz3j7lx4fXjv5djVXSDUrbWjFbcskDEPc/qP1sjWG5FTSy2tGrrQBnjxzIMFdBrS/RrFK8/ODC9EJ'
 
 #Configurações adicionais
 region_name = 'us-east-1'
@@ -39,14 +39,15 @@ def criar_bucket(aws_access_key_id, aws_secret_access_key, session_token, region
             
 
 def enviar_arquivo(aws_access_key_id, aws_secret_access_key, session_token, bucket_name, file_path, object_name):
-    s3_client = boto3.client('s3', 
+    
+    s3 = boto3.client('s3', 
                         aws_access_key_id = aws_access_key_id,
                         aws_secret_access_key = aws_secret_access_key,
                         aws_session_token = session_token)
                         
     try:
         s3.upload_file(file_path, bucket_name, object_name)
-        print(f"o Upload do arquivo '{object_name}' no Bucket '{bucket_name}'")
+        print(f"o Upload do arquivo '{object_name}' no Bucket '{bucket_name}' foi realizado com sucesso.")
     
     #Possíveis erros:
     except FileNotFoundError:
@@ -58,10 +59,10 @@ def enviar_arquivo(aws_access_key_id, aws_secret_access_key, session_token, buck
     except Exception as exc:
         print(f"Erro Inesperado: {exc}")
         
-    response = criar_bucket(aws_access_key_id, aws_secret_access_key, session_token, region_name, bucket_name)
+response = criar_bucket(aws_access_key_id, aws_secret_access_key, session_token, region_name, bucket_name)
     
-    if response:
-        print("Informações sobre a criação do bucket:", response)
+if response:
+    print("Informações sobre a criação do bucket:", response)
         
 
 enviar_arquivo(aws_access_key_id, aws_secret_access_key, session_token, bucket_name, file_path, object_name)
