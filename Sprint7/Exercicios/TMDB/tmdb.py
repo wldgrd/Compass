01049bfb7e6c3e25151ef920649f4771ec401962
Welder Leite Garrido <weldergarrido@gmail.com>
2025-01-19 19:58:38 -0300

    exercícios

--- Sprint7/Exercicios/TMDB/tmdb.py
+++ Sprint7/Exercicios/TMDB/tmdb.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from IPython.display import display 
 
-api_key = "44a51a00d9f1c02bb28e87d10f642984"
+api_key = input('Insira sua chave API do TMDb: ')
 
 url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=pt-BR"
 
@@ -12,11 +12,13 @@ data = response.json()
 filmes = []
 
 for movie in data['results']:
-	df = {'Titulo': movie['title'],
+	df = {
+	'Titulo': movie['title'],
 	'Data de lançamento': movie['release_date'],
 	'Visão geral': movie['overview'],
 	'Votos': movie['vote_count'],
-	'Média dos votos': movie['vote_average']}
+	'Média dos votos': movie['vote_average']
+	}
 	
 	filmes.append(df)
 	
